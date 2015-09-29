bcf23c1d0dcb89fa1d4590c34ae654a363fe2897
Pierre Laferriere <polaferriere@gmail.com>
2015-09-28 21:32:20 -0400

    initial commit

---
+++ tests/hello-test.js
@@ -0,0 +1,13 @@
+var expect = chai.expect;
+
+describe("delete me", function(){
+  it("should be deleted", function(){
+    expect(true).to.equal(true);
+  });
+});
+
+describe("Test", function(){
+  it("should return true", function(){
+    expect(true).to.equal(true);
+  });
+});
