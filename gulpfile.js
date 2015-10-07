bd1aa4b9172fbe76d0ffaa81900c9dacd0a1c10d
Pierre Laferriere <polaferriere@gmail.com>
2015-10-07 11:21:32 -0400

    Added Font-Awesome requirements and functionality

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,8 @@ gulp.task('images', function(){
 });
 
 gulp.task('fonts', function() {
-  return gulp.src('app/fonts/**/*')
+  return gulp.src(['app/fonts/**/*', 'bower_components/font-awesome/fonts/*.+(eot|svg|ttf|woff|woff2|otf)'])
+  .pipe(gulp.dest('.tmp/fonts'))
   .pipe(gulp.dest('dist/fonts'))
 });
 
@@ -187,7 +188,7 @@ gulp.task('deploy', function() {
 });
 
 gulp.task('default', function (callback) {
-  runSequence(['styles', 'scripts', 'templates', 'wiredep', 'wiredep:tests', 'tests:scripts', 'browserSync', 'watch'], callback);
+  runSequence(['styles', 'fonts', 'scripts', 'templates', 'wiredep', 'wiredep:tests', 'tests:scripts', 'browserSync', 'watch'], callback);
 });
 
 function onError(error){
